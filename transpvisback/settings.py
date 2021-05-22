dd7a61234f974a52ab5dce008306aaf8fc020555
abdennour001 <amokranabdennour@gmail.com>
2021-05-22 16:53:14 +0100

    add allowed mehods

--- transpvisback/settings.py
+++ transpvisback/settings.py
@@ -47,6 +47,14 @@ CORS_ORIGIN_WHITELIST = (
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
 
 ALLOWED_HOSTS = ["*"]
 
