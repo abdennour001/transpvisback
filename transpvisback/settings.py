7079eb0f9f1a66544b3ac361e7c913edb6336bbb
abdennour001 <amokranabdennour@gmail.com>
2021-05-29 23:27:31 +0100

    add static files

--- transpvisback/settings.py
+++ transpvisback/settings.py
@@ -177,7 +177,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
